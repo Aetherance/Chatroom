88c18ab535d58e3bde9d08c3752c221b69d86107
The_INK <2085163736@qq.com>
2025-07-25 16:23:43 +0800

    update README.md

--- run_cli.sh
+++ run_cli.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 if ! docker image inspect "chatroom-cli:latest" >/dev/null 2>&1; then
+########################################################
   wget http://$1:9090/download/chatroom-cli.tar
   docker load < chatroom-cli.tar
-  rm chatroom-cli.tar
+  rm chatroom-cli.tar                                  
+########################################################
 fi
+
+########################################################
 
 if [ ! -d "/tmp/history" ]; then
   mkdir /tmp/history
@@ -21,3 +25,5 @@ fi
 clear
 
 docker run -it --rm -v "/tmp/history:/Chatroom/history" -v "/tmp/download:/Chatroom/download" -v "/tmp/upload:/Chatroom/upload" chatroom-cli:latest ./client $1
+
+########################################################
