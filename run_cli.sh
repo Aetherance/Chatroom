f97ce4ee86a63d566c096c8428244271841fafb0
The_INK <2085163736@qq.com>
2025-07-28 15:52:03 +0800

    修改运行脚本

--- run_cli.sh
+++ run_cli.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-if ! docker image inspect "chatroom-cli:latest" >/dev/null 2>&1; then
+if ! docker image inspect "chatroom-cli:v1.0" >/dev/null 2>&1; then
 
-#  获取客户端镜像  ########################################
+#  获取客户端镜像
   wget http://$1:9090/download/chatroom-cli.tar
   docker load < chatroom-cli.tar
   rm chatroom-cli.tar                                  
-########################################################
 fi
 
 
-#  运行客户端镜像  ########################################
-
+#  运行客户端镜像
 if [ ! -d "/tmp/history" ]; then
   mkdir /tmp/history
 fi
@@ -26,6 +24,4 @@ fi
 
 clear
 
-docker run -it --rm -v "/tmp/history:/Chatroom/history" -v "/tmp/download:/Chatroom/download" -v "/tmp/upload:/Chatroom/upload" chatroom-cli:latest ./client $1
-
-########################################################
+docker run -it --rm -v "/tmp/history:/Chatroom/history" -v "/tmp/download:/Chatroom/download" -v "/tmp/upload:/Chatroom/upload" chatroom-cli:v1.0 ./client $1
